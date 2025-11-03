18445893d5fdb96d5b5d6b7cc28e90b6e6956f95
Shruti <shruti.dhameliya@dignizant.com>
2025-11-03 18:27:50 +0530

    feat: Implement quest completion and user progress tracking with activity logging

---
+++ devquest-backend/src/routes/progressRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const auth = require('../middleware/auth')
+const { query, param } = require('express-validator')
+const {
+  completeQuest,
+  getUserProgress,
+  getActivityLog,
+} = require('../controllers/progressController')
+const router = express.Router()
+
+router.post(
+  '/complete/:questId',
+  auth,
+  [param('questId').isMongoId()],
+  completeQuest,
+)
+router.get('/my-progress', auth, getUserProgress)
+router.get(
+  '/activity-log',
+  auth,
+  [
+    query('page').optional().isInt({ min: 1 }),
+    query('limit').optional().isInt({ min: 1, max: 100 }),
+  ],
+  getActivityLog,
+)
+
+module.exports = router
--- devquest-backend/src/server.js
+++ devquest-backend/src/server.js
@@ -4,13 +4,14 @@ const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
 const cors = require('cors')
 const questRoutes = require('./routes/questRoutes')
+const progressRoutes = require('./routes/progressRoutes')
 
 dotenv.config()
 const app = express()
 
 app.use(
   cors({
-    origin: 'http://localhost:3000', // your Next.js frontend
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // if you plan to send cookies
@@ -27,10 +28,11 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/auth', authRoutes)
 app.use('/api/quests', questRoutes)
+app.use('/api/progress', progressRoutes)
 
 // connect to db
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log('connected to db listening on port', process.env.PORT)
